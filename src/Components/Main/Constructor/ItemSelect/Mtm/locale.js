caf90bcbfa44c557e1032f32b45444dd119b5036
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-09 01:00:38 +0300

    locales added

---
+++ src/Components/Main/Constructor/ItemSelect/Mtm/locale.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Алексей on 09.07.2017.
+ */
+
+export const locale = {
+    en: {
+        show: "show",
+        hide: "hide",
+        add: "add",
+        delete: "delete"
+    },
+    ru:{
+        show: "показать",
+        hide: "спрятать",
+        add: "добавить",
+        delete: "удалить"
+    }
+};
