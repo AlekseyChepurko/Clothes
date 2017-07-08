6abd6a8ffd7c82851a216be80da723ced9fe08d5
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-08 03:33:46 +0300

    build scripts configured

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Алексей on 07.07.2017.
+ */
+
+module.exports = {
+    plugins: {
+        'postcss-font-magician': {
+            foundries: ['google']
+        },
+        "postcss-media-minmax": {},
+        'postcss-nested': {},
+        'autoprefixer': {},
+        'cssnano': {}
+    }
+};
