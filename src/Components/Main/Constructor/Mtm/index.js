7aa21634a8f2325c73bd47c523176110df915a61
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-08 19:09:14 +0300

    section added

--- src/Components/Main/Constructor/Mtm/index.js
+++ src/Components/Main/Constructor/Mtm/index.js
@@ -1,3 +1,34 @@
 /**
  * Created by Алексей on 08.07.2017.
  */
+
+import React, {Component} from 'react'
+import Isvg from 'react-inlinesvg'
+import './main.css'
+
+const items = [
+    'jacket',
+    'shirt',
+    'trousers',
+    'coat',
+    'vest',
+    'tie'
+];
+let path;
+
+const Item = (item, index)=>{
+    path = require(`./items/${item}.png`);
+    return <li key={index} styleName="mtm__item-wrap">
+        <img src={path} styleName={`${item} item`}/>
+    </li>
+    };
+
+class Mtm extends Component {
+    render(){
+        return <ul styleName="common">
+            {items.map((item,index) => Item(item, index))}
+        </ul>
+    }
+}
+
+export default Mtm;
---
+++ src/Components/Main/Constructor/Rtw/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Алексей on 08.07.2017.
+ */
+
+import React from 'react'
+
+const Rtw  = ()=><div>Rtw section</div>;
+
+export default Rtw;
