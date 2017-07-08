9a46dd6a25254bbe9d6be4bffcaf1febacf0709a
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-08 21:26:42 +0300

    wrapping ItemSelect component added ( added mtm and rtw select)

---
+++ src/Components/Main/Constructor/ItemSelect/index.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Алексей on 08.07.2017.
+ */
+import React, {Component} from 'react'
+import Mtm from './Mtm'
+import Rtw from './Rtw'
+import './main.css'
+
+class ItemSelect extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            mtmChoose: true
+        }
+    };
+    render(){
+        return <section styleName="common">
+            <section styleName="constructor__choice-wrap">
+                <button
+                    style={{color: this.state.mtmChoose ? "#8f7548": null}}
+                    styleName={`constructor__choice`}
+                    onClick={(()=>{this.setState({mtmChoose: true})}).bind(this)}
+                >mtm</button>
+
+                <button
+                    style={{color: !this.state.mtmChoose ? "#8f7548": null}}
+                    styleName={`constructor__choice`}
+                    onClick={(()=>{this.setState({mtmChoose: false})}).bind(this)}
+                >rtw</button>
+            </section>
+            <section>
+                {this.state.mtmChoose
+                    ? <Mtm />
+                    : <Rtw />
+                }
+            </section>
+        </section>
+    }
+}
+
+export default ItemSelect
