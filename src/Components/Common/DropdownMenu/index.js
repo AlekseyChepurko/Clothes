308bf06b833299158025b3bc5b37e63fad836f76
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-09 21:20:23 +0300

    initial component added

---
+++ src/Components/Common/DropdownMenu/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Алексей on 09.07.2017.
+ */
+import React, {Component} from 'react'
+import './main.css'
+
+class DropdownMenu extends Component {
+    render() {
+        return <ul>
+
+        </ul>
+    }
+}
+
+DropdownMenu.defaultProps = {};
+
+export default DropdownMenu
