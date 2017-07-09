377b89e05aaf4ba5f392e9ab8dd2c0cacea5abb0
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-09 20:37:57 +0300

    added connection to store. if side menu is open -> dashboard is not rendered

--- src/Components/Common/Header/LeftMenu/index.js
+++ src/Components/Common/Header/LeftMenu/index.js
@@ -2,12 +2,17 @@
  * Created by Алексей on 08.07.2017.
  */
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
 import './main.css'
 class LeftMenu extends Component {
     render(){
+        const {sideMenuIsOpen} = this.props;
         return <section styleName="common">
             <a styleName="order__button"><p>order</p></a>
-            <p styleName="dashboard bold">dashboard</p>
+            {sideMenuIsOpen
+                ? null
+                : <p styleName="dashboard bold">dashboard</p>
+            }
             <p styleName="bold">ed {this.props.ed}</p>
         </section>
     }
@@ -16,5 +21,6 @@ class LeftMenu extends Component {
 LeftMenu.defaultProps = {
     ed: "18.08.2017"
 };
+const mapStateToProps = (state)=>({sideMenuIsOpen: state.sideMenu.isOpen});
 
-export default LeftMenu;
+export default connect(mapStateToProps)(LeftMenu);
