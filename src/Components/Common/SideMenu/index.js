fe887bc6609b19bc2d3b8bed9e30920c3e7e5ebf
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-09 21:00:50 +0300

    Content component added

--- src/Components/Common/SideMenu/index.js
+++ src/Components/Common/SideMenu/index.js
@@ -4,12 +4,26 @@
 
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import './main.css'
+import Navigation from "./Navigation";
+import SearchBar from './SearchBar'
+import Content from './Content'
 
 class SideMenu extends Component {
     render(){
-        return <nav></nav>
+        const {isOpen} = this.props;
+
+        return <section
+            styleName={`common ${isOpen ? 'open' : ''}`}
+            className='noWidth'>
+            <div styleName="menu-wrap">
+                <Navigation />
+                <SearchBar />
+                <Content/>
+            </div>
+        </section>
     }
 }
-const mapStateToProps = (state)=>({});
+const mapStateToProps = (state)=>({isOpen: state.sideMenu.isOpen});
 
 export default connect(mapStateToProps)(SideMenu)
