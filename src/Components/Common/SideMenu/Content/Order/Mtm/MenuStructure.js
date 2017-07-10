66e8089b4bacfb62cb26aa98bde08a55994b7bdd
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-10 04:55:59 +0300

    localization commit

---
+++ src/Components/Common/SideMenu/Content/Order/Mtm/MenuStructure.js
@@ -0,0 +1,119 @@
+/**
+ * Created by Алексей on 09.07.2017.
+ */
+
+export const menu = {
+    en: [
+        {
+            text: 'trouser 1',
+            subItem: [{
+                text: 'some text'
+            }]
+        },
+        {
+            text: 'trouser 2',
+            subItem: [{
+                text: 'some text'
+            }]
+        },
+        {
+            text: 'shirt',
+            subItem: [{
+                text: 'some text'
+            }]
+        },
+        {
+            text: 'coat',
+            subItem: [{
+                text: 'some text'
+            }]
+        },
+        {
+            text: 'coat 2',
+            subItem: [{
+                text: 'some text'
+            }]
+        },
+        {
+            text: 'jacket 1',
+            subItem: [{
+                text: 'some text'
+            }]
+        },
+        {
+            text: 'jacket 2',
+            subItem: [{
+                text: 'some text'
+            }]
+        },
+        {
+            text: 'jacket 3',
+            subItem: [{
+                text: 'some text'
+            }]
+        },
+        {
+            text: 'jacket 4',
+            subItem: [{
+                text: 'some text'
+            }]
+        },
+    ],
+    ru:
+        [
+            {
+                text: 'брюки 1',
+                subItem: [{
+                    text: 'русский текст'
+                }]
+            },
+            {
+                text: 'брюки 2',
+                subItem: [{
+                    text: 'русский текст'
+                }]
+            },
+            {
+                text: 'рубашка',
+                subItem: [{
+                    text: 'русский текст'
+                }]
+            },
+            {
+                text: 'пальто',
+                subItem: [{
+                    text: 'русский текст'
+                }]
+            },
+            {
+                text: 'пальто 2',
+                subItem: [{
+                    text: 'русский текст'
+                }]
+            },
+            {
+                text: 'пиджак 1',
+                subItem: [{
+                    text: 'русский текст'
+                }]
+            },
+            {
+                text: 'пиджак 2',
+                subItem: [{
+                    text: 'русский текст'
+                }]
+            },
+            {
+                text: 'пиджак 3',
+                subItem: [{
+                    text: 'русский текст'
+                }]
+            },
+            {
+                text: 'пиджак 4',
+                subItem: [{
+                    text: 'русский текст'
+                }]
+            },
+        ],
+};
--- src/Components/Common/SideMenu/Content/Order/Mtm/index.js
+++ src/Components/Common/SideMenu/Content/Order/Mtm/index.js
@@ -2,14 +2,19 @@
  * Created by Алексей on 09.07.2017.
  */
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import DropdownMenu from 'Common/DropdownMenu'
+import {menu} from './MenuStructure'
 import './main.css'
 
 class Mtm extends Component {
     render() {
-        return <sectoin>mtm content</sectoin>
+        return <DropdownMenu menu={menu[this.props.lang]} />
     }
 }
 
-Mtm.defaultProps = {};
+Mtm.defaultProps = {
+    lang: 'en'
+};
 
-export default Mtm
+export default connect((state)=>({lang: state.language.lang}))(Mtm)
