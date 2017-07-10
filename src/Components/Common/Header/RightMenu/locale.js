f91ce8a8eb089586d69df5829b35c4aad2d880a7
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-10 04:53:17 +0300

    locale added

---
+++ src/Components/Common/Header/RightMenu/locale.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Алексей on 10.07.2017.
+ */
+
+export const locale ={
+    en: {
+        money: "$",
+        lang: 'language'
+    },
+    ru: {
+        money: '₽',
+        lang: 'язык'
+    }
+};
