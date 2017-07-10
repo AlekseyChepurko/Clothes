d26baeeebf2488d50086137c2f4dacc3bde45d5c
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-10 04:51:41 +0300

    locale added

---
+++ src/Components/Common/Header/LeftMenu/locale.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Алексей on 10.07.2017.
+ */
+
+export const locale = {
+    en:{
+        dashboard: 'dashboard',
+        order: 'order'
+    },
+    ru:{
+        dashboard: 'личный кабинет',
+        order: 'заказ'
+    }
+};
