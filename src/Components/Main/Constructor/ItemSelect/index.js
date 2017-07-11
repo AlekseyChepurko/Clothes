27f58c1685d26ccd4ff214539af06038b4b9f3d7
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-11 04:10:21 +0300

    code refactor

--- src/Components/Main/Constructor/ItemSelect/index.js
+++ src/Components/Main/Constructor/ItemSelect/index.js
@@ -2,34 +2,45 @@
  * Created by Алексей on 08.07.2017.
  */
 import React, {Component} from 'react'
-import Mtm from './Mtm'
-import Rtw from './Rtw'
+import Mtm from './Content/Mtm'
+import Rtw from './Content/Rtw'
 import './main.css'
+
+class Item  extends Component {
+    render(){
+        const {name, active, onClick} = this.props;
+        return <button
+            style={{color: name===active ? "#8f7548": null}}
+            styleName={`constructor__choice`}
+            onClick={()=>{
+                console.log(name);
+                onClick(name)}
+            }
+        >{name}</button>
+    }
+}
 
 class ItemSelect extends Component {
     constructor(props){
         super(props);
         this.state = {
-            mtmChoose: true
-        }
+            activeTab: "mtm"
+        };
+        this.changeActive = this.changeActive.bind(this);
+    };
+    changeActive(active){
+        this.setState({
+            activeTab: active
+        });
     };
     render(){
         return <section styleName="common">
             <section styleName="constructor__choice-wrap">
-                <button
-                    style={{color: this.state.mtmChoose ? "#8f7548": null}}
-                    styleName={`constructor__choice`}
-                    onClick={(()=>{this.setState({mtmChoose: true})}).bind(this)}
-                >mtm</button>
-
-                <button
-                    style={{color: !this.state.mtmChoose ? "#8f7548": null}}
-                    styleName={`constructor__choice`}
-                    onClick={(()=>{this.setState({mtmChoose: false})}).bind(this)}
-                >rtw</button>
+                <Item name="mtm" active={this.state.activeTab} onClick={this.changeActive}/>
+                <Item name="rtw" active={this.state.activeTab} onClick={this.changeActive} />
             </section>
             <section>
-                {this.state.mtmChoose
+                {this.state.activeTab === "mtm"
                     ? <Mtm />
                     : <Rtw />
                 }
