c733b22b5bcf589a58f67357308babdabd89a19c
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-11 05:08:56 +0300

    image added; toggle function added

--- src/Components/Main/Constructor/ItemSelect/HideButton/index.js
+++ src/Components/Main/Constructor/ItemSelect/HideButton/index.js
@@ -2,14 +2,23 @@
  * Created by Алексей on 11.07.2017.
  */
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {toggleItemsSelectMenu} from 'Root/actions'
+
+import arrow from './images/arrow.png'
 import './main.css'
 
 class HideButton extends Component {
     render() {
-        return <button></button>
+        return <button
+            onClick={this.props.toggleItemsSelectMenu}
+            styleName="select__visibility-button">
+            <img src={arrow} alt=""/>
+        </button>
     }
 }
 
 HideButton.defaultProps = {};
 
-export default HideButton
+const mapState = (state)=>({});
+export default connect(mapState, {toggleItemsSelectMenu})(HideButton)
