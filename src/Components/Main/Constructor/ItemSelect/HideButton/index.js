d2316d8109476cd22e9586dbfb494ceafcb5cb4b
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-11 04:10:49 +0300

    basic commit

---
+++ src/Components/Main/Constructor/ItemSelect/HideButton/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Алексей on 11.07.2017.
+ */
+import React, {Component} from 'react'
+import './main.css'
+
+class HideButton extends Component {
+    render() {
+        return <button></button>
+    }
+}
+
+HideButton.defaultProps = {};
+
+export default HideButton
