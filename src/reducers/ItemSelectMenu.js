8fe3fea787f480a10868ee85b5f61d66d20ba550
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-11 08:17:21 +0300

    CLOSE_ITEMS_SELECT_MENU case added

--- src/reducers/ItemSelectMenu.js
+++ src/reducers/ItemSelectMenu.js
@@ -2,7 +2,7 @@
  * Created by Алексей on 11.07.2017.
  */
 
-import {TOGGLE_ITEMS_SELECT_MENU} from '../constants/ActionTypes'
+import {TOGGLE_ITEMS_SELECT_MENU,CLOSE_ITEMS_SELECT_MENU} from '../constants/ActionTypes'
 
 const initialState = {
     isOpen: true
@@ -11,6 +11,7 @@ const initialState = {
 const itemSelectMenu = (state = initialState, action)=>{
     switch (action.type){
         case TOGGLE_ITEMS_SELECT_MENU: return {isOpen: !state.isOpen};
+        case CLOSE_ITEMS_SELECT_MENU: return {isOpen: false};
         default:
             return state;
     }
