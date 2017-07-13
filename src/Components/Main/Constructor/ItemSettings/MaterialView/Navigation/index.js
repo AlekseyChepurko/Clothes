3128dd26b82786558a799480a189ed2d30321e64
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-14 01:05:01 +0300

    chosenItems array get from store; locale added

--- src/Components/Main/Constructor/ItemSettings/MaterialView/Navigation/index.js
+++ src/Components/Main/Constructor/ItemSettings/MaterialView/Navigation/index.js
@@ -2,15 +2,18 @@
  * Created by Алексей on 08.07.2017.
  */
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {locale} from './locale'
 import './main.css'
 
 class Navigation extends Component {
     render() {
+        const {chosenItems, activeItem, lang} = this.props;
         return <ul styleName="wrap">
-            {this.props.chosenItems.map((item,index)=>
-                <li styleName={`item${item.active?' active':''}`}
+            {chosenItems.map((item,index)=>
+                <li styleName={`item${activeItem.name === item.name ?' active':''}`}
                     key={index}>
-                        {item.name}
+                        {locale[lang][item.name]}
                 </li>)}
         </ul>
     }
@@ -20,4 +23,9 @@ Navigation.defaultProps = {
     chosenItems: [{name: 'trousers', active: true}, {name: 'jacket'}, {name:'coat'}]
 };
 
-export default Navigation
+const mapStateToProps = (state)=>({
+    lang: state.language.lang,
+    chosenItems: state.itemSelectMenu.addedItems,
+    activeItem: state.itemSelectMenu.activeItem,
+});
+export default connect(mapStateToProps)(Navigation)
