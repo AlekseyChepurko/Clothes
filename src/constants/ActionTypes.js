98fb7105691368fec01bdfe40234a926596488b8
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-13 23:37:21 +0300

    SET_ACTIVE_ITEM action added

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -2,8 +2,16 @@
  * Created by Алексей on 09.07.2017.
  */
 
+// SideMenu
 export const OPEN_SIDE_MENU = 'OPEN_SIDE_MENU';
 export const CLOSE_SIDE_MENU = 'CLOSE_SIDE_MENU';
+
+// Language
 export const CHANGE_LANGUAGE = 'CHANGE_LANGUAGE';
+
+// Select menu toggle
 export const TOGGLE_ITEMS_SELECT_MENU = 'TOGGLE_ITEMS_SELECT_MENU';
 export const CLOSE_ITEMS_SELECT_MENU = 'CLOSE_ITEMS_SELECT_MENU';
+
+// Item select
+export const SET_ACTIVE_ITEM = 'SET_ACTIVE_ITEM';
--- src/reducers/ItemSelectMenu.js
+++ src/reducers/ItemSelectMenu.js
@@ -2,16 +2,28 @@
  * Created by Алексей on 11.07.2017.
  */
 
-import {TOGGLE_ITEMS_SELECT_MENU,CLOSE_ITEMS_SELECT_MENU} from '../constants/ActionTypes'
+import {
+    SET_ACTIVE_ITEM,
+    TOGGLE_ITEMS_SELECT_MENU,
+    CLOSE_ITEMS_SELECT_MENU} from '../constants/ActionTypes'
 
 const initialState = {
-    isOpen: true
+    isOpen: true,
+    activeItem: {}
 };
 
 const itemSelectMenu = (state = initialState, action)=>{
     switch (action.type){
-        case TOGGLE_ITEMS_SELECT_MENU: return {isOpen: !state.isOpen};
-        case CLOSE_ITEMS_SELECT_MENU: return {isOpen: false};
+        case TOGGLE_ITEMS_SELECT_MENU: return {
+            ...state,
+            isOpen: !state.isOpen};
+        case CLOSE_ITEMS_SELECT_MENU: return {
+            ...state,
+            isOpen: false};
+        case SET_ACTIVE_ITEM: return {
+            ...state,
+            activeItem: action.item};
+
         default:
             return state;
     }
