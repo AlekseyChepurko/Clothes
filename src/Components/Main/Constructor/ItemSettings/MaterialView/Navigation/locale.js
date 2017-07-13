3269a1562a24b2b701c955bdbac7a638a284fe48
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-14 01:02:13 +0300

    locale file added

---
+++ src/Components/Main/Constructor/ItemSettings/MaterialView/Navigation/locale.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Алексей on 14.07.2017.
+ */
+
+
+export const locale = {
+    en: {
+        jacket: 'jacket',
+        shirt: 'shirt',
+        trousers: 'trousers',
+        coat: 'coat',
+        vest: 'vest',
+        tie: 'tie'
+    },
+    ru: {
+        jacket: 'пиджак',
+        shirt: 'рубашка',
+        trousers: 'брюки',
+        coat: 'пальто',
+        vest: 'жилет',
+        tie: 'галстук'
+    }
+};
