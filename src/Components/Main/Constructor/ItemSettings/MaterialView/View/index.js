aa3f5c020ea0bc85bbb545b95bcf3b07e4cf4c99
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-14 04:46:23 +0300

    display component added

--- src/Components/Main/Constructor/ItemSettings/MaterialView/View/index.js
+++ src/Components/Main/Constructor/ItemSettings/MaterialView/View/index.js
@@ -2,13 +2,21 @@
  * Created by Алексей on 08.07.2017.
  */
 import React, {Component} from 'react'
+import Display from './Display'
 import Footer from './Footer'
+import im from 'Static/images/materials/type1/599.png'
 import './main.css'
 
 class View extends Component {
     render() {
         return <section styleName="material__view-wrap">
-            <section styleName="view"></section>
+
+            <Display styleName="view" />
+            {/*<section styleName="view">*/}
+                {/*<img src={im} alt=""/>*/}
+                {/*<img src={im} alt=""/>*/}
+
+            {/*</section>*/}
             <Footer/>
         </section>
     }
