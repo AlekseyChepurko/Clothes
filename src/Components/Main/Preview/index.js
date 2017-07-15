377da99fde124e2de997d01c3b521509922cb1b9
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-15 05:49:42 +0300

    basic image rotation added

--- src/Components/Main/Preview/index.js
+++ src/Components/Main/Preview/index.js
@@ -5,10 +5,34 @@ import React, {Component} from 'react'
 import defaultImage from 'Static/images/default.png'
 import './main.css'
 
+const path='/static/images/views/0/1/';
+
 class Preview extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            img: 0,
+            animate: true
+        };
+        this.toggle=()=>()=>{
+            this.setState({animate: !this.state.animate})
+        };
+    }
+
+    // TODO clear timer interval
+    componentDidMount(){
+        this.animateInterval = setInterval(
+            ()=>{
+                if (this.state.animate){
+                    this.setState({
+                        img: (this.state.img+1)%90
+                    })
+                }
+            },500);
+    }
     render() {
-        return <section styleName="preview-wrap">
-            <img src={defaultImage} styleName="preview__dummy-img" alt="dummy"/>
+        return <section styleName="preview-wrap" onClick={this.toggle()}>
+            <img src={`${path}${this.state.img}.png`} styleName="preview__dummy-img" alt="dummy"/>
         </section>
     }
 }
