09f6966839eccb84f909fab86584269b516bc695
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-18 04:38:52 +0300

    Refactor: fetching order structure from .json added to componentWillMount method

--- src/Components/Main/Constructor/index.js
+++ src/Components/Main/Constructor/index.js
@@ -4,11 +4,17 @@
 
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {actions} from 'Root/actions'
 import ItemSelect from './ItemSelect'
 import ItemSettings from './ItemSettings'
+import items from 'Root/constants/items.json'
 import './main.css'
 class Constructor extends Component {
-
+    componentWillMount(){
+        fetch(items).then(res=>res.json()).then((res)=>{
+            this.props.setOrder(res);
+        })
+    }
     render(){
         return <section styleName="common">
             {this.props.sideMenuIsOpen
@@ -19,4 +25,4 @@ class Constructor extends Component {
     }
 }
 const mapStateToProps = (state)=>({sideMenuIsOpen: state.sideMenu.isOpen});
-export default connect(mapStateToProps)(Constructor);
+export default connect(mapStateToProps,{...actions})(Constructor);
