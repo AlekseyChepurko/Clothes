5451ef0ad4d896ed4fa90437764cff63238f1c84
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-20 06:57:24 +0300

    Display order for constructor footer added

---
+++ src/constants/ItemOptionsDisplayOrder.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Алексей on 20.07.2017.
+ */
+
+
+export const displayOrder = [
+    'look',
+    'fason',
+    'material',
+    'silhouette',
+    'lapel', 'edges' ,'pockets'
+];
