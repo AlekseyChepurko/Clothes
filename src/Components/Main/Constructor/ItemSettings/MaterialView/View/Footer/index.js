1907758b87b2ac7766879c3809984281b4898eb7
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-20 06:58:02 +0300

    getPathByStructureObject function added

--- src/Components/Main/Constructor/ItemSettings/MaterialView/View/Footer/index.js
+++ src/Components/Main/Constructor/ItemSettings/MaterialView/View/Footer/index.js
@@ -3,8 +3,11 @@
  */
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import expect from 'expect'
 import _ from 'lodash'
 import FooterPathItem from './FooterPathItem'
+import items from 'Static/images/logos/res.json'
+import {displayOrder} from 'Root/constants/ItemOptionsDisplayOrder'
 import '../main.css'
 
 class Footer extends Component {
@@ -12,7 +15,6 @@ class Footer extends Component {
         const {activeItem, structure} = this.props;
 
         const pathToShow = _.find(structure, item => item.name === activeItem.name) ? _.find(structure, item => item.name === activeItem.name) : [];
-        console.log(pathToShow);
         return <ul styleName="footer__wrap">
             <FooterPathItem {...pathToShow}/>
              {/*{this.props.path.map((item, index)=>*/}
@@ -39,3 +41,58 @@ const mapStateToProps = state => ({
     structure: state.orderStrucrure
 });
 export default connect(mapStateToProps, {})(Footer)
+
+function flattStructureObject(structureObject={}){
+    return _.flatten([
+        structureObject.parameters
+            ?   [{
+            name: structureObject.name,
+            text: ''
+        }]
+            :   [],
+        ...structureObject.subChoice
+            ? structureObject.subChoice.map(item =>
+                flattStructureObject(item)
+            )
+            :[]
+    ])
+}
+function getPathByStructureObject(object){
+    return sortByOrderArray(_.uniqBy(flattStructureObject(object), o=>o.name), displayOrder)
+}
+function sortByOrderArray(target=[],orderArray=displayOrder){
+    return target.sort( (a,b) =>
+        orderArray.indexOf(a.name) >= orderArray.indexOf(b.name) ? 1 : -1
+    )
+}
+if(process.env.NODE_ENV !== 'production'){
+
+    fetch(items).then(r=>r.json()).then(structureObject => {
+        expect(getPathByStructureObject(structureObject[1])).toEqual([
+            {
+                name: 'look',
+                text: ''
+            },
+            {
+                name: 'fason',
+                text: ''
+            },
+            {
+                name: 'silhouette',
+                text: ''
+            },
+            {
+                name: 'lapel',
+                text: ''
+            },
+            {
+                name: 'edges',
+                text: ''
+            },
+            {
+                name: 'pockets',
+                text: ''
+            }
+        ])
+    })
+}
