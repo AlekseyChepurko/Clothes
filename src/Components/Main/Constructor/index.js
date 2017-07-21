6d955a2b84012ef4b67babce027d217c379313a2
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-07-21 06:09:23 +0300

    order initialization added

--- src/Components/Main/Constructor/index.js
+++ src/Components/Main/Constructor/index.js
@@ -4,6 +4,7 @@
 
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import _ from 'lodash'
 import {actions} from 'Root/actions'
 import ItemSelect from './ItemSelect'
 import ItemSettings from './ItemSettings'
@@ -11,12 +12,33 @@ import ItemSettings from './ItemSettings'
 import items from 'Static/images/logos/res.json'
 import './main.css'
 
+function getInitialItemOrder(itemStructure){
+    const {subChoice, parameters} = itemStructure;
+    const res = [];
+    if (parameters){
+        res.push({name: itemStructure.name, value: parameters[0]});
+    }
+    if(subChoice) {
+        subChoice.forEach((e)=>{
+                res.push(getInitialItemOrder(e));
+        })
+    }
+    return _.uniqBy(_.flattenDeep(res), e=>e.name);
+}
+
 class Constructor extends Component {
     componentWillMount(){
         fetch(items).then(res=>res.json()).then((res)=>{
             this.props.initOrderStructure(res);
+            res.forEach(e=>{
+                this.props.addItem({
+                    name: e.name,
+                    parameters: getInitialItemOrder(e)
+                });
+            });
         })
     }
+    initOrder(){}
     render(){
         return <section styleName="common">
             {this.props.sideMenuIsOpen
