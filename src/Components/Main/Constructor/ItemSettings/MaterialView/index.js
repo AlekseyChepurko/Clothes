361e58148a6b71017116321dbd74df20faa7fd43
chep <aleksey.chepurko@gmail.com>
2017-07-21 03:01:49 +0300

    refactor: Navigation presentational(!)-component props added

--- src/Components/Main/Constructor/ItemSettings/MaterialView/index.js
+++ src/Components/Main/Constructor/ItemSettings/MaterialView/index.js
@@ -2,17 +2,35 @@
  * Created by Алексей on 08.07.2017.
  */
 import React,{Component} from 'react'
+import {connect} from 'react-redux'
+import {actions} from 'Root/actions'
 import Navigation from './Navigation'
 import View from './View'
 import './main.css'
 
 class MaterialView extends Component {
     render(){
+        const {items,activeItem, lang, itemSelectMenuIsOpen, setActiveItem} = this.props;
         return <section styleName="wrap">
-            <Navigation/>
+            <Navigation
+                {...{
+                    items,
+                    lang,
+                    activeValue: activeItem.name,
+                    itemSelectMenuIsOpen,
+                    setActiveItem
+                }}
+            />
             <View/>
         </section>
     }
 }
 
-export default MaterialView
+
+const mapStateToProps = state => ({
+    itemSelectMenuIsOpen: state.Constructor.itemSelectMenu.isOpenAfterAnimation,
+    lang: state.language.lang,
+    items: state.order,
+    activeItem: state.Constructor.activeItem,
+});
+export default connect(mapStateToProps, {...actions})(MaterialView)
