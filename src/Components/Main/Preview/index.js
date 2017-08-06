6c6379068338d8b46b6b45ddd75ded24fce5a0ca
aleksey chepurko <aleksey.chepurko@gmail.com>
2017-08-06 19:56:13 +0300

    dummy scaling added

--- src/Components/Main/Preview/index.js
+++ src/Components/Main/Preview/index.js
@@ -2,7 +2,6 @@
  * Created by Алексей on 08.07.2017.
  */
 import React, {Component} from 'react'
-import defaultImage from 'Static/images/default.png'
 import './main.css'
 
 const path='/static/images/views/0/1/';
@@ -11,27 +10,70 @@ const path='/static/images/views/0/1/';
 const getNextImageNumber = (currentNumber, imageCount)=>1 + (currentNumber)%imageCount;
 
 class Preview extends Component {
+
     constructor(props){
         super(props);
         this.state = {
+            mouseDown: false,
             img: 1,
-            animate: true
+            animate: true,
+            scaleCoefficient: props.scaleCoefficient,
+            imageYOffset: 0,
         };
-        this.startAnimation=()=>()=>{
-            this.setState({animate: true});
-            this.animateInterval = setInterval(
-                ()=>{
-                    this.setState({
-                        img: getNextImageNumber(this.state.img, this.props.imageCount)
-                    })
+        this.startAnimation=()=>{
+            if (this.animateInterval) return;
+            this.setState({animate: true},()=>{
+                this.animateInterval = setInterval(
+                    ()=>{
+                        this.setState({
+                            img: getNextImageNumber(this.state.img, this.props.imageCount)
+                        })
 
-                },this.props.speed);
+                    },this.props.speed);
+            });
         };
-        this.stopAnimation=()=>(e)=>{
-            e.preventDefault();
+        this.stopAnimation = ()=>{
             this.setState({animate: false});
-            clearInterval(this.animateInterval)
+            clearInterval(this.animateInterval);
+            this.animateInterval = undefined;
         };
+        this._onMouseDown = ()=> e=>{
+            e.preventDefault();
+            this.setState({mouseDown: true});
+            this.stopAnimation(e)
+        };
+        this._onMouseUp = ()=> e=>{
+            this.setState({mouseDown: false,scaleCoefficient: 1, imageYOffset: 0, lastPos: 0});
+            this.startAnimation(e)
+        };
+        this._onMouseMove=()=>e=>{
+            if(this.state.mouseDown)
+                this.setState({lastPos: e.nativeEvent.offsetY});
+        };
+        this._onMouseLeave = () =>{
+            this.startAnimation();
+            this.setState({
+                mouseDown: false,
+            });
+        };
+        this.onWheel=()=>(e)=>{
+            e.stopPropagation();
+            const roundedCoefficient = Math.round(this.state.scaleCoefficient * 10) / 10;
+            if (roundedCoefficient === 1.0 && e.deltaY>0 || roundedCoefficient >= this.props.maxScale && e.deltaY<0) return;
+            const wheelUp = e.deltaY < 0;
+            const vla = e.target.getBoundingClientRect().top >0 ? e.target.getBoundingClientRect().top : 0;
+            const offsetTop = e.clientY-vla;
+            this.setState({
+                imageYOffset: offsetTop,
+                scaleCoefficient:
+                    roundedCoefficient === 1.0
+                    && !wheelUp
+                    || roundedCoefficient >= this.props.maxScale
+                    && wheelUp
+                        ? roundedCoefficient
+                        : this.state.scaleCoefficient- Math.round(e.deltaY*this.props.scaleRate*10)/10
+            });
+        }
     }
 
     // TODO clear timer interval
@@ -56,15 +98,27 @@ class Preview extends Component {
         }
         return <section
             styleName="preview-wrap"
-            onMouseUp={this.startAnimation()}
-            onMouseDown={this.stopAnimation()}>
-            {/*<img src={`${path}${this.state.img}.png`} styleName="preview__dummy-img" alt="dummy"/>*/}
-            {imgs}
+            onMouseLeave={this._onMouseLeave}
+            onMouseMove={this._onMouseMove()}
+            onMouseUp={this._onMouseUp()}
+            onMouseDown={this._onMouseDown()}>
+            <div
+                onWheelCapture={this.onWheel()}
+                style={{
+                    position: 'relative',
+                    transformOrigin: `50% ${this.state.imageYOffset}px`,
+                    transform: `scale(${this.state.scaleCoefficient}) `
+                }}>
+                {imgs}
+            </div>
         </section>
     }
 }
 
 Preview.defaultProps = {
+    scaleCoefficient: 1,
+    maxScale: 3,
+    scaleRate: 0.001,
     speed: 75,
     imageCount: 89
 };
